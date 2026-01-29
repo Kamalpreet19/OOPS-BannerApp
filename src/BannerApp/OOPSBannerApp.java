0d0812c4ae0d865e48043aab640bd88adddc6daa
Kamalpreet Kaur <kamalpreet19@gmail.com>
2026-01-29 11:15:13 +0530

    UC3-Print OOPS Banner

--- src/BannerApp/OOPSBannerApp.java
+++ src/BannerApp/OOPSBannerApp.java
@@ -1,17 +1,30 @@
 package BannerApp;
 /**
- * OOPSBannerApp UC2-OOPS Banner Display Application
+ * OOPSBannerApp UC3-OOPS Banner Display Application
  *
  */
 public class OOPSBannerApp {
     public static void main(String args[]) {
-        System.out.println("*********" + "   " + "*********" + "   " + "*********" + "   " + "*********");
-        System.out.println("*       *" + "   " + "*       *" + "   " + "*       *" + "   " + "*        ");
-        System.out.println("*       *" + "   " + "*       *" + "   " + "*       *" + "   " + "*        ");
-        System.out.println("*       *" + "   " + "*       *" + "   " + "*********" + "   " + "*********");
-        System.out.println("*       *" + "   " + "*       *" + "   " + "*        " + "   " + "        *");
-        System.out.println("*       *" + "   " + "*       *" + "   " + "*        " + "   " + "        *");
-        System.out.println("*********" + "   " + "*********" + "   " + "*        " + "   " + "*********");
+        System.out.println(String.join("   ",
+                "*********", "*********", "*********", "*********"));
+
+        System.out.println(String.join("   ",
+                "*       *", "*       *", "*       *", "*        "));
+
+        System.out.println(String.join("   ",
+                "*       *", "*       *", "*       *", "*        "));
+
+        System.out.println(String.join("   ",
+                "*       *", "*       *", "*********", "*********"));
+
+        System.out.println(String.join("   ",
+                "*       *", "*       *", "*        ", "        *"));
+
+        System.out.println(String.join("   ",
+                "*       *", "*       *", "*        ", "        *"));
+
+        System.out.println(String.join("   ",
+                "*********", "*********", "*        ", "*********"));
 
 
         System.out.println();
