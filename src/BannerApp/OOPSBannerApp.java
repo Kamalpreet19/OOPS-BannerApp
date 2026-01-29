3a2ebb9daf21448a5f9956370ed5448864c01e77
Kamalpreet Kaur <kamalpreet19@gmail.com>
2026-01-29 10:59:47 +0530

    Initial commit

---
+++ src/BannerApp/OOPSBannerApp.java
@@ -0,0 +1,10 @@
+package BannerApp;
+/**
+ * OOPSBannerApp UC1-OOPS Banner Display Application
+ *
+ */
+public class OOPSBannerApp {
+    public static void main(String args[]) {
+        System.out.println("OOPS");
+    }
+}
